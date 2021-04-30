bdbf1b6826123bbce759fe8f443eb4dc7dda7ebe
SaltyAom <aomkirby123@gmail.com>
2021-04-30 22:10:16 +0700

    :tada: feat - add ingress

--- k8s/down.sh
+++ k8s/down.sh
@@ -1,2 +1,3 @@
 kubectl delete -f api.yaml
 kubectl delete -f deployment.yaml
+kubectl delete -f ingress.yaml
--- k8s/up.sh
+++ k8s/up.sh
@@ -1,2 +1,3 @@
 kubectl apply -f api.yaml
 kubectl apply -f deployment.yaml
+kubectl apply -f ingress.yaml
